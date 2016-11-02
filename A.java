f72da3aa4326d311587004b2c3259c52ebf11537
vamshi8395 <vamshi.manohar@gmail.com>
2016-11-02 16:55:20 +0530

    Add files via upload

---
+++ A.java
@@ -0,0 +1,13 @@
+class A
+{
+public static void main(String args[])
+{
+int i;
+{
+for(i=1;i<20;i++)
+{
+System.out.println(i);
+}
+}
+}
+}
---
+++ B.java
@@ -0,0 +1,13 @@
+class B 
+{
+public static void main(String arg[])
+{
+int a;
+{
+for(a=20;a<=100;a++)
+{
+System.out.println("vamshi");
+}
+}
+}
+}
